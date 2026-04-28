6ab48f3d68b0e0eb669791e3ddc43caba0c60844
sejal <sakharwsejal@gmail.com>
2026-04-28 12:12:50 +0530

    Login folder and file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-
-
+import { RouterProvider } from "react-router"
+import {router} from './app.routes.jsx'
 
 
 const App = () => {
   return (
-    <>
+    <RouterProvider router={router}>
     
-    </>
+    </RouterProvider>
   )
 }
 
