af564d37b9a7fe931bf30ebd40093e8df32fc4f9
sejal <sakharwsejal@gmail.com>
2026-04-29 08:49:01 +0530

    register and login ui completed

--- src/features/auth/pages/Login.jsx
+++ src/features/auth/pages/Login.jsx
@@ -1,25 +1,33 @@
 import "../auth.form.scss"
+import { useNavigate,Link } from "react-router";
 
 
 const Login = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return (
         <main>
             <div className="form-container">
                 <h1>Login</h1>
-                <form>
+                <form onSubmit={handleSubmit}>
 
                     <div className="input-group">
                         <label htmlFor="email">Email</label>
-                        <input type="email" id="email" name="email" placeholder="Enter email address"/>
+                        <input type="email" id="email" name="email" placeholder="Enter email address" />
                     </div>
 
                     <div className="input-group">
                         <label htmlFor="password">Password</label>
-                        <input type="password" id="password" name="password" placeholder="Enter email address"/>
+                        <input type="password" id="password" name="password" placeholder="Enter email address" />
                     </div>
 
                     <button className="button primary-button">Login</button>
                 </form>
+                <p>haven't an Account? <Link to={'/register'}>Register</Link></p>
+
             </div>
         </main>
     )
